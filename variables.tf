87e07e16c0d6f81270014d1c18486e73545a575a
Jim Nguyen <JimNguyen@state.gov>
2026-02-07 18:45:54 -0500

    w

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 variable "resource_group_name" {
-    type = string
-    description = "Name of the resource group"
-    
+  type        = string
+  description = "Name of the resource group"
+
 }
 variable "location" {
-    type = string
-    description = "Location of the resource group"
-    
+  type        = string
+  description = "Location of the resource group"
+
 }
 variable "storage_account_name" {
-    type = string
-    description = "Name of the storage account"
-    
+  type        = string
+  description = "Name of the storage account"
+
 }
 variable "environment" {
-    type = string
-    description = "Environment of the storage account"
-  
+  type        = string
+  description = "Environment of the storage account"
+
 }
